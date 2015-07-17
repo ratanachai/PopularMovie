44df5f8389bd2e8c965ffdd08e2317afc08aba45
Ratanachai S <ratanachai@gmail.com>
2015-07-17 23:10:24 +0700

    Set Poster Image into ImageView; Organize layout with nested LinearLayout with layout_weight

--- app/src/main/java/com/ratanachai/popularmovies/DetailActivityFragment.java
+++ app/src/main/java/com/ratanachai/popularmovies/DetailActivityFragment.java
@@ -6,7 +6,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 
 /**
@@ -25,10 +28,16 @@ public class DetailActivityFragment extends Fragment {
         Intent intent = getActivity().getIntent();
         if (intent != null && intent.hasExtra("strings")){
             String[] movieInfo = intent.getStringArrayExtra("strings");
+
+            // Set all TextView and Poster
             ((TextView) rootview.findViewById(R.id.movie_title)).setText(movieInfo[0]);
             ((TextView) rootview.findViewById(R.id.movie_overview)).setText(movieInfo[2]);
-            ((TextView) rootview.findViewById(R.id.movie_rating)).setText(movieInfo[3]);
-            ((TextView) rootview.findViewById(R.id.movie_release)).setText(movieInfo[4]);
+            ((TextView) rootview.findViewById(R.id.movie_rating)).append(" "+movieInfo[3]+"/10");
+            ((TextView) rootview.findViewById(R.id.movie_release)).append(" "+movieInfo[4]);
+            Picasso.with(getActivity())
+                    .load("http://image.tmdb.org/t/p/w185" + movieInfo[1])
+                    .into((ImageView)rootview.findViewById(R.id.movie_poster));
+
         }
 
         return rootview;
