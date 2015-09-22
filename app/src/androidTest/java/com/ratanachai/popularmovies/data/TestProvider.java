6c5b2f40ea36eaca4a2a9d4bbb7fe9e3f0302798
Ratanachai S <ratanachai@gmail.com>
2015-09-23 05:31:18 +0700

    Add MovieProvider to AndroidManifest

---
+++ app/src/androidTest/java/com/ratanachai/popularmovies/data/TestProvider.java
@@ -0,0 +1,33 @@
+package com.ratanachai.popularmovies.data;
+
+import android.content.ComponentName;
+import android.content.pm.PackageManager;
+import android.content.pm.ProviderInfo;
+import android.test.AndroidTestCase;
+
+/**
+ * Created by keng on 15/09/22.
+ */
+public class TestProvider extends AndroidTestCase {
+
+    public void testProviderRegistry() {
+        PackageManager pm = mContext.getPackageManager();
+
+        // We define the component name based on the package name from the context and the Provider class.
+        ComponentName componentName = new ComponentName(mContext.getPackageName(), MovieProvider.class.getName());
+        try {
+            // Fetch the provider info using the component name from the PackageManager
+            // This throws an exception if the provider isn't registered.
+            ProviderInfo providerInfo = pm.getProviderInfo(componentName, 0);
+
+            // Make sure that the registered authority matches the authority from the Contract.
+            assertEquals("Error: Provider registered with authority: " + providerInfo.authority +
+                            " instead of authority: " + MovieContract.CONTENT_AUTHORITY,
+                    providerInfo.authority, MovieContract.CONTENT_AUTHORITY);
+        } catch (PackageManager.NameNotFoundException e) {
+
+            assertTrue("Error: Provider not registered at " + mContext.getPackageName(), false);
+        }
+    }
+
+}
