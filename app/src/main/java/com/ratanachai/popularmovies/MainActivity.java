72294f8f30e2680ee413fe4b56e20678f1b45a56
Ratanachai S <ratanachai@gmail.com>
2016-05-24 21:46:51 +1200

    Remove Dummy fragment

--- app/src/main/java/com/ratanachai/popularmovies/MainActivity.java
+++ app/src/main/java/com/ratanachai/popularmovies/MainActivity.java
@@ -8,12 +8,8 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity
@@ -193,24 +189,6 @@ public class MainActivity extends AppCompatActivity
                 case 1: return "Highest Vote";
                 default: return "Watchlist";
             }
-        }
-    }
-
-    /**
-     * A dummy fragment representing a section of the app, but that simply displays dummy text.
-     */
-    public static class DummySectionFragment extends Fragment {
-
-        public static final String ARG_SECTION_NUMBER = "section_number";
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_section_dummy, container, false);
-            Bundle args = getArguments();
-            ((TextView) rootView.findViewById(android.R.id.text1)).setText(
-                    getString(R.string.dummy_section_text, args.getInt(ARG_SECTION_NUMBER)));
-            return rootView;
         }
     }
 
